05ca1fb54517ea2b68e9c2a780d38ad285b5f208
StefanZhang <stefanzhang@protonmail.com>
2020-09-25 23:02:05 -0400

    Updates on Parsing Json, and nections on Mysql.

--- src/main/scala/Stefanzhang/com/Twitter.scala
+++ src/main/scala/Stefanzhang/com/Twitter.scala
@@ -3,6 +3,7 @@ package Stefanzhang.com
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import com.alibaba.fastjson.JSON
+import java.sql.{Connection,DriverManager}
 
 object Twitter {
 
@@ -22,20 +23,31 @@ object Twitter {
 
     lines.foreachRDD(x=>{
       x.foreach(t=>{
-        //Class.forName("com.mysql.jdbc.Driver")
-        //val conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Twitter", "root", "123456")
-        //println(t)
+        Class.forName("com.mysql.jdbc.Driver")
+        val conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Twitter", "root", "stefan")
         try {
+          //remove duplicates
+          val sql1 = String.format("delete t1 FROM tweets t1 INNER  JOIN tweets t2 WHERE t1.id < t2.id AND t1.name = t2.name;")
+          conn.prepareStatement(sql1).executeUpdate()
+
+          //parse json
           val json = JSON.parseObject(t)
           val create = json.getString("created_at")
-          val name = json.getJSONObject("user").getString("name")
-          val sql = String.format("insert into spark values(\"%s\",\"%s\")",create,name)
-          println(name)
-          //conn.prepareStatement(sql).executeUpdate()
+          val name = json.getJSONObject("user").getString("screen_name")
+          val text = json.getString("text")
+          val source = json.getString("source")
+
+          //TODO.. Sentiment Analysis
+          val senti = "pos"
+
+          //insert new vals
+          val sql2 = String.format("INSERT INTO Twitter.tweets (time, name, text, sentiment, source) VALUES (\'%s\',\'%s\', \'%s\', \'%s\', \'%s\');",create,name, text, senti, source)
+          conn.prepareStatement(sql2).executeUpdate()
+
         }catch {
           case e => e.printStackTrace()
         }finally {
-          //conn.close()
+          conn.close()
         }
       })
     })
